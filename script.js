fd923325483a1b1d1bbd9ba1524b78b436918def
Andres-Ventura <andres.venturase@gmail.com>
2022-09-25 09:51:50 -0700

    dropdown update

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const main = document.querySelector("#main-card");
+const icon = document.querySelector("#rectangle img#logo");
+const exit = document.querySelector("#exit");
+console.log(exit)
 
+icon.addEventListener("click", () => {
+    if  (main.style.display !== "none"){
+        main.style.display = "none";
+    } else {
+        main.style.display = "block"
+    }
+} )
+
+exit.addEventListener("click", () => {
+        main.style.display = "none";
+    } 
+)
